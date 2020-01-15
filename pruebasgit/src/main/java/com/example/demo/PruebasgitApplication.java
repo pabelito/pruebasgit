e90e1338aedb41d79b5223f694b78019d44e3aee
Pabel Bautista <pabelbc5@gmail.com>
2020-01-15 15:46:58 -0500

    pruebas satisfactorias

---
+++ pruebasgit/src/main/java/com/example/demo/PruebasgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasgitApplication.class, args);
+	}
+
+}
